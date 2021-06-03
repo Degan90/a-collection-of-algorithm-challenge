316214f6ca081376d78d0959e60a7ae08548f575
Degan90 <83658327+Degan90@users.noreply.github.com>
2021-06-03 10:33:31 -0400

    Update app.js

--- largest NUMBERS/app.js
+++ largest NUMBERS/app.js
@@ -1,1 +1,6 @@
 
+const largestNumber = (num) => {
+    if (num === 0) return 0;
+    if(num > 0) return parseInt('9'.repeat(num));
+    else if (num < 0) return parseFloat('.' + '9'.repeat(num * -1));
+}
